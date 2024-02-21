60b96390c74a9962c948c91182411ef005eb552b
Jacky Andrazat <82565381+Jackyandrazat@users.noreply.github.com>
2024-02-21 14:41:58 +0700

    Fix: Setup Package react App
    Setup React and tailwind and daisi UI

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  // eslint-disable-next-line no-undef
+  plugins: [require("daisyui")],
+}
+
