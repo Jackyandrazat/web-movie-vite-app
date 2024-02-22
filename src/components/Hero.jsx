7bef9e48e32e6219efc96b883d2caea2b602c721
Jacky Andrazat <82565381+Jackyandrazat@users.noreply.github.com>
2024-02-22 13:36:40 +0700

    fix:add hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,63 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+function Hero() {
+  return (
+    <div>
+      <div
+        className="hero min-h-96"
+        style={{
+          backgroundImage:
+            "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            {/* <button className="btn btn-primary">Get Started</button> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function HeroFigure() {
+  return (
+    <div>
+      <div className="hero min-h-96 bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <img
+            src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+          <img
+            src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+          <img
+            src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">List of Interesting Films</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <a href="/filmlist" className="btn btn-primary">More Film..</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { Hero, HeroFigure };
