c30985bd89e3ef2a193ed3e6883391f72c5b6ae3
Jacky Andrazat <82565381+Jackyandrazat@users.noreply.github.com>
2024-02-22 13:39:10 +0700

    fix: add pages Film List

---
+++ src/pages/FilmList.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import CardList from "../components/CardList";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+export default function FilmList() {
+  return (
+    <div>
+      <Navbar />
+      <CardList />
+      <Footer />
+    </div>
+  );
+}
